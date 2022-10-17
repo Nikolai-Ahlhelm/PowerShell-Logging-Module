0b313c28de06b6ec9c6d71b6656f7d2e4b968c82
Nikolai <44299417+Nikolai-Ahlhelm@users.noreply.github.com>
2022-10-17 15:03:40 +0200

    Update 2.3

--- basic_logging.ps1
+++ basic_logging.ps1
@@ -1,36 +1,62 @@
 ### Basic Powershell Logging
-#28.04.2022 - v2.2
+#17.10.2022 - v2.3
 
 ### USAGE
 #Import Module:		Import-Module -Name .\basic_logging.ps1
 
-#Create Logger Obj:		$Log = New-Object -TypeName Logger -ArgumentList "log.txt"
+#Create Log Obj:	$Log = New-Object -TypeName Logger -ArgumentList (LogFileName:"log.txt",PrintToConsole:$TRUE)
 
 #LogEntry:			$Log.Entry("Info", "Test Message") ## $Log.Entry(TYPE, MESSAGE)
 
+#Logging Types: 	Default(all except debug), Debug(all), Productive(error,crit), Error(only errors), Critical(only critical), None(no logs)
 
 class Logger
 {
-    [string] $LogFilePath 
-    [string] $LogDate
-    [string] $LogMessage
-    [string] $LogType
-    [string] $format
-    [bool] $PrintToConsole
+    [string] $LogFilePath 	#Path to log file 
+    [string] $LogDate		#Date used for log entries
+    [string] $LogMessage	#Message for log entries
+    [string] $LogType		#Logging type (Productive, Debug, etc.)
+    [string] $format		#Timestamp format
+    [bool] 	 $PrintToConsole	#Should entries be printed out to console
+	#LogTypeGroups
+	[string] $LTDefault
+	[string] $LTDebug
+	[string] $LTProductive
+	[string] $LTError
+	[string] $LTCritical
 
+	#Constructor
     Logger(
         [string] $logFilePath,
+		[string] $logType
         [bool] $PrintToConsole
-    ){
+    )
+	{
         $this.logFilePath = $logFilePath
         $this.LogDate = Get-Date -Format "dd/MM/yyyy"
-		#Get-Date -Format "dd-MM-yyyy-hh:mm:ss:ffff"
 		$this.format = "dd/MM/yyyy-hh:mm:ss:ffff"
-        if($null -eq $PrintToConsole){
-            $this.PrintToConsole = $TRUE
+		### Eval. Log Type Fnc
+        if($null -eq $PrintToConsole)
+		{
+			$this.PrintToConsole = $TRUE
         } else {
             $this.PrintToConsole = $PrintToConsole
         }
+		
+        if($null -eq $logType)
+		{
+			$logType = "DEFAULT"
+        }		
+		
+		#LogTypeGroups
+		$this.LTDefault 	= "ERROR","INFO","WARNING","CRITICAL"
+		$this.LTDebug 		= "ERROR","INFO","WARNING","CRITICAL","DEBUG"
+		$this.LTProductive 	= "ERROR","INFO","CRITICAL"
+		$this.LTError 		= "ERROR"
+		$this.LTCritical 	= "CRITICAL"
+		#Get logtype 
+		$this.LogType = EvalLogType($logType)
+		
         
     }
 
@@ -46,6 +72,32 @@ class Logger
         $this.PrintToConsole = $BOOL
     }
 
+	[string] EvalLogType($INPUT) {
+		if ($Type -ieq "DEFAULT" -or $Type -ieq "DEF") {
+            $type = "DEFAULT"
+        }
+		elseif ($Type -ieq "DEBUG" -or $Type -ieq "DBG") {
+            $type = "DEBUG"
+        }
+		elseif ($Type -ieq "PRODUCTIVE" -or $Type -ieq "PROD") {
+            $type = "PRODUCTIVE"
+        }
+		elseif ($Type -ieq "ERROR" -or $Type -ieq "ERR") {
+            $type = "ERROR"
+        }
+		elseif ($Type -ieq "CRITICAL" -or $Type -ieq "CRIT") {
+            $type = "CRITICAL"
+        }
+		elseif ($Type -ieq "NONE") {
+            $type = "NONE"
+        }
+		else
+		{
+			$type = "DEFAULT"
+		}
+		return $type
+	}
+
     [string] Entry($Type,$Message) {
         if ($Type -ieq "ERROR" -or $Type -ieq "err" -or $Type -ieq "e") {
             $type = "ERROR"
@@ -59,10 +111,60 @@ class Logger
         elseif ($Type -ieq "CRIT" -or $Type -ieq "critical" -or $Type -ieq "c") {
             $type = "CRITICAL"
         }
+		elseif ($Type -ieq "DBG" -or $Type -ieq "debug" -or $Type -ieq "d") {
+            $type = "DEBUG"
+        }
         else {
             $type = $Type
         }
 
+
+		$messageType = $type
+		
+		#LogType Filter
+		if ($this.LogType -ieq "DEFAULT" -and $LTDefault -contains $messageType)
+		{
+			$this.WriteLog($type,$Message)
+		}
+		elseif ($this.LogType -ieq "DEBUG" -and $LTDebug -contains $messageType)
+		{
+			$this.WriteLog($type,$Message)
+		}
+		elseif ($this.LogType -ieq "PRODUCTIVE" -and $LTProductive -contains $messageType)
+		{
+			$this.WriteLog($type,$Message)
+		}
+		elseif ($this.LogType -ieq "ERROR" -and $LTError -contains $messageType)
+		{
+			$this.WriteLog($type,$Message)
+		}		
+		elseif ($this.LogType -ieq "CRITICAL" -and $LTCritical -contains $messageType)
+		{
+			$this.WriteLog($type,$Message)
+		}
+		else
+		{
+			#Nothing happens : Type = None
+			return ""
+		}
+		
+		<#
+		$this.LogMessage = "["+(Get-Date -Format $this.format)+"] [$type] "+$Message
+
+		Write-Output $this.LogMessage
+        Out-File $this.logFilePath -Append -InputObject $this.LogMessage
+		
+        if($this.PrintToConsole) {
+            return $this.LogMessage
+        } 
+        else {
+            return ""
+        }#>
+
+    }
+	
+	[string] WriteLog($type,$Message)
+	{
 		$this.LogMessage = "["+(Get-Date -Format $this.format)+"] [$type] "+$Message
 
 		Write-Output $this.LogMessage
@@ -74,7 +176,6 @@ class Logger
         else {
             return ""
         }
-
-    }
+	}
 
 }
--- example.ps1
+++ example.ps1
@@ -3,9 +3,10 @@ Import-Module -Name .\basic_logging.ps1
 #$Log = [Logger]::new("log.txt")
 
 #Arguments: LogPath, PrintToConsole
-$Log = New-Object -TypeName Logger -ArgumentList ("log.txt",$TRUE)
+$Log = New-Object -TypeName Logger -ArgumentList ("log.txt","DEFAULT",$TRUE)
 
 
-$Log.Entry("Info", "Test Message")
+$Log.Entry("DEBUG", "Debug Test Message")
+$Log.Entry("Info", "Info Test Message")
 
 pause
